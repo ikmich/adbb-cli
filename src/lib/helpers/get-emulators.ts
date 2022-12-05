c920c309888428f2d495a7bbc02c189226ad37ab
ikmich <michfx@gmail.com>
2022-12-05 22:43:06 +0100

    Fix emulator command

--- src/lib/helpers/get-emulators.ts
+++ src/lib/helpers/get-emulators.ts
@@ -1,16 +1,10 @@
 import execShellCmd from './exec-shell-cmd.js';
 
 const getEmulators = async (): Promise<string[]> => {
-  let results: string[] = [];
+  // let results: string[] = [];
   const output = await execShellCmd('emulator -list-avds');
 
-  const rexGlobal = /\w+/gim;
-  const emulators: any = output.match(rexGlobal);
-  if (emulators && emulators.length > 0) {
-    results = emulators;
-  }
-
-  return results;
+  return output.split(/\n+/);
 };
 
 export default getEmulators;
